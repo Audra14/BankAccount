2706848caeb738ed9d42a6d21367419584ac3f32
LJ <ljf5124@psu.edu>
2017-04-13 17:44:16 -0400

    Added some stuff

--- src/banksystem/ServerHandler.java
+++ src/banksystem/ServerHandler.java
@@ -5,9 +5,12 @@
  */
 package banksystem;
 
+import java.io.BufferedInputStream;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.Socket;
 import java.util.ArrayList;
 
@@ -22,11 +25,28 @@ public class ServerHandler extends Thread {
     
     public ServerHandler(Socket socket) {
         this.socket = socket;
-
-       
     }
 
     public void run() {
-        
+        try {
+            while (true) {
+                InputStream in = socket.getInputStream();
+                DataInputStream input = new DataInputStream(in);
+                OutputStream out = socket.getOutputStream();
+                DataOutputStream output = new DataOutputStream(out);
+                String message = input.readUTF();
+                if(message.charAt(0) == 0){ //If Balance
+                    output.writeUTF("Balance");
+                } else if(message.charAt(0) == 1){ //If Deposit
+                    output.writeUTF("Deposit Successful");
+                } else if(message.charAt(0) == 2){ //If Withdrawal
+                    output.writeUTF("Withdrawal Successful");
+                } else { //If transfer
+                    output.writeUTF("Transfer Successful");
+                }
+            }
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
     }
 }
