7a82dc70d14337fd55221880fefa0f4c542b6a9c
Audra14 <astafursky@client-104-39-40-235.mobility.psu.edu>
2017-04-13 18:51:33 -0400

    More stuff

--- src/banksystem/ServerHandler.java
+++ src/banksystem/ServerHandler.java
@@ -30,10 +30,12 @@ public class ServerHandler extends Thread {
     public void run() {
         try {
             while (true) {
+                
                 InputStream in = socket.getInputStream();
                 DataInputStream input = new DataInputStream(in);
                 OutputStream out = socket.getOutputStream();
                 DataOutputStream output = new DataOutputStream(out);
+                
                 String message = input.readUTF();
                 System.out.println(message);
                 String accountNumber = message.substring(0, 3);
@@ -45,23 +47,33 @@ public class ServerHandler extends Thread {
                 User u = new User("name");
                 a.addBalance(100);
                 char specifier = message.charAt(5);
+                
                 if(specifier == '0'){ //If Balance
-                    output.writeUTF(Double.toString(a.getBalance()));
+                    
+                    output.writeUTF("Your balance: " + Double.toString(a.getBalance()));
+                    
                 } else if(specifier == '1'){ //If Deposit
+                    
                     amount = Double.parseDouble(message.substring(7, message.length()));
                     t = new Deposit(u, a, amount);
                     t.start();
                     output.writeUTF("Deposit Successful. New balance: " + a.getBalance());
+                    
                 } else if(specifier == '2'){ //If Withdrawal
+                    
                     amount = Double.parseDouble(message.substring(7, message.length()));
                     t = new Withdrawal(u, a, amount);
                     t.start();
                     output.writeUTF("Withdrawal Successful. New balance: " + a.getBalance());
+                    
                 } else { //If transfer
+                    
                     String newAccount = message.substring(7, 10);
                     amount = Double.parseDouble(message.substring(12, message.length()));
                     t = new Transfer(u, a, amount, newAccount);
+                    t.start();
                     output.writeUTF("Transfer Successful");
+                    
                 }
             }
         } catch(IOException e) {
