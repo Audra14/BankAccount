8c684c95ffb2b1ebe9ccb14fb279d0ca6fb77d12
LJ <ljf5124@psu.edu>
2017-04-18 18:01:48 -0400

    Making dummy data

---
+++ src/banksystem/Accounts.java
@@ -0,0 +1,16 @@
+package banksystem;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author ljf5124
+ */
+public class Accounts implements Serializable{
+    public static ArrayList<Account> list;
+    
+    public Accounts(){
+        list = new ArrayList<Account>();
+    }
+}
