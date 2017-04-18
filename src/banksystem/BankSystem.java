2a1e5c150e151ad19917ae0fa9d4b3fb84d37960
LJ <ljf5124@psu.edu>
2017-04-18 17:33:45 -0400

    Making dummy data

--- src/banksystem/BankSystem.java
+++ src/banksystem/BankSystem.java
@@ -13,7 +13,7 @@ public class BankSystem {
     
     public static void main(String[] args) throws IOException {
         
-        AccountList list = AccountList.getAccountList();
+        makeDummyData();
         System.out.println("The bank server is running.");
         ServerSocket listener = new ServerSocket(PORT);
         try {
@@ -25,6 +25,19 @@ public class BankSystem {
         } finally {
             listener.close();
         }
+    }
+    
+    public static void makeDummyData(){
+        Account a1 = new Account("1111");
+        a1.addBalance(100);
+        Account a2 = new Account("2222");
+        a2.addBalance(200);
+        Account a3 = new Account("3333");
+        a3.addBalance(300);
+        AccountList.getAccountList().list.add(a1);
+        AccountList.getAccountList().list.add(a2);
+        AccountList.getAccountList().list.add(a3);
+        AccountList.makeList();
     }
 
 
