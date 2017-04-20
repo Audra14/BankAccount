ed1650e4201c253181ad8d7c5c0b3fc86368482d
LJ <ljf5124@psu.edu>
2017-04-20 17:25:30 -0400

    Amounts set to client should be correct

--- src/banksystem/ServerHandler.java
+++ src/banksystem/ServerHandler.java
@@ -68,7 +68,11 @@ public class ServerHandler extends Thread {
                     
                     amount = Double.parseDouble(message.substring(7, message.length()));
                     t = new Deposit(a, amount);
-                    t.start();
+                    try{
+                        t.join();
+                    }catch(InterruptedException e){
+                        e.printStackTrace();
+                    }
                     output.writeUTF("Deposit Successful. New balance: " + a.getBalance());
                     this.list.makeList();
                     
@@ -76,7 +80,11 @@ public class ServerHandler extends Thread {
                     
                     amount = Double.parseDouble(message.substring(7, message.length()));
                     t = new Withdrawal(a, amount);
-                    t.start();
+                    try{
+                        t.join();
+                    }catch(InterruptedException e){
+                        e.printStackTrace();
+                    }
                     output.writeUTF("Withdrawal Successful. New balance: " + a.getBalance());
                     this.list.makeList();
                     
@@ -85,7 +93,11 @@ public class ServerHandler extends Thread {
                     Account newAccount = new Account(message.substring(7, 10));
                     amount = Double.parseDouble(message.substring(12, message.length()));
                     t = new Transfer(a, amount, newAccount);
-                    t.start();
+                    try{
+                        t.join();
+                    }catch(InterruptedException e){
+                        e.printStackTrace();
+                    }
                     output.writeUTF("Transfer Successful");
                     this.list.makeList();
                     
